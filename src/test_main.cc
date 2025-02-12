ef6cac60cb59b8832fd76bf8c1d8d18a7aa5a139
JunseoMin <minjs4562@gmail.com>
2025-02-12 22:14:00 +0900

    testcode

--- src/test_main.cc
+++ src/test_main.cc
@@ -1,27 +1,80 @@
-#include <radar_utills.hpp>
+#include <iostream>
+#include <vector>
+#include <Eigen/Dense>
+#include <pmc/pmc.h>
 
-int main(void){
-  cv::Mat radar;
-  cv::Mat radar_cart;
+using namespace std;
+using namespace Eigen;
+
+// Eigen::MatrixXd를 CSR 형식으로 변환하는 함수
+void convertToCSR(const MatrixXd& G, vector<double>& values, vector<int>& column_indices, vector<int>& row_ptr) {
+    int rows = G.rows();
+    int cols = G.cols();
+    
+    row_ptr.push_back(0); // 첫 번째 행의 시작 인덱스
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            if (G(i, j) != 0) { // 0이 아닌 요소만 저장
+                values.push_back(G(i, j));
+                column_indices.push_back(j);
+            }
+        }
+        row_ptr.push_back(values.size()); // 다음 행의 시작 인덱스
+    }
+}
+
+pmc::pmc_graph EigenToPMC(const Eigen::MatrixXd& G){
+  vector<long long> vs;
+  vector<int>       es;
+
+  vs.push_back(0);
+
+  int tmp = 0;
+
+  for (int vertex = 0; vertex < G.rows(); vertex++)
+  {
+    int n_edges =  G.row(vertex).sum();
+    tmp += n_edges;
+    vs.push_back(tmp);
+    for (int edge_idx = 0; edge_idx < G.cols(); edge_idx ++){
+      if (G(vertex,edge_idx) == 1)
+      {
+        es.push_back(edge_idx);
+      }
+    }
+  }
   
-  std::vector<double> azimuths;
-  std::vector<int64_t> timestemps;
+  // std::cout << "vs: " << std::endl;
+  // for (int v : vs) cout << v << " ";
+  // std::cout << std::endl;
+  // std::cout << "es: " << std::endl;
+  // for (int e : es) cout << e << " ";
+  // std::cout << std::endl;
 
-  getRadarOnce("/home/junseo/datasets/MURLAn/drive-download-20250206T045839Z-002/mulan_oxford/polar_oxford_form/1564718964139967887.png", timestemps, azimuths,radar);
-  PolarToCartesian(azimuths,radar,radar_cart);
+  pmc::pmc_graph p_G(vs,es);
+  return p_G;
+}
+ 
+int main() {
+    // 예제 행렬 (희소 행렬)
+    MatrixXd G(4, 4);
+    G << 0, 1, 0, 1,
+         1, 0, 1, 1,
+         0, 1, 0, 0,
+         1, 1, 0, 0;
 
-  // viz = eigenToCvMat<double>(radar);
-  // cart_viz = eigenToCvMat<double>(radar_cart);
+    vector<double> values;
+    vector<int> column_indices;
+    vector<int> row_ptr;
 
-  cv::imshow("radar raw", radar);
-  // cv::imshow("radar cart",cart_viz);
+    std::cout << "original edges: " << G.sum() / 2 << std::endl;
+    std::cout << "original verti: " << G.rows() << std::endl;
+    convertToCSR(G, values, column_indices, row_ptr);
+    pmc::pmc_graph pg = EigenToPMC(G);
 
-  cv::Mat resizedImg;
-  cv::resize(radar_cart, resizedImg, cv::Size(1024, 1024)); // (width=800, height=600)
+    std::cout << "calc num edges: " << pg.num_edges() << std::endl;
+    std::cout << "calc num verti: " << pg.num_vertices() << std::endl;
 
-  cv::imshow("Radar Cart", resizedImg);
-
-  cv::waitKey(0);
-
-  return 0;
+    return 0;
 }
