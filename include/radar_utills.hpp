b7a13eaca2fddb1c0959992b0a59bb5fdd8e2d7e
JunseoMin <minjs4562@gmail.com>
2025-02-13 16:54:57 +0900

    feat add data getter; add radar type

--- include/radar_utills.hpp
+++ include/radar_utills.hpp
@@ -9,7 +9,32 @@
 #include <opencv2/opencv.hpp>
 #include <vector>
 
+#include <filesystem>
+
+struct RadarFFT{
+  std::vector<int64_t> timestamp;
+  std::vector<double> azimuths;
+  cv::Mat fft;
+
+  RadarFFT() = default;
+  RadarFFT(std::vector<int64_t>& timestamp, std::vector<double>& azimuths, cv::Mat& fft)
+  :timestamp(timestamp), azimuths(azimuths), fft(fft){}
+};
+
+struct RadarCartesian{
+  std::vector<int64_t> timestamp;
+  std::vector<double> azimuths;
+  cv::Mat cart_radar;
+
+  RadarCartesian() = default;
+  RadarCartesian(std::vector<int64_t>& timestamp, std::vector<double>& azimuths, cv::Mat& cart)
+  :timestamp(timestamp), azimuths(azimuths), cart_radar(cart){}
+};
+
 void getRadarOnce(const std::string& filepath, std::vector<int64_t> &timestamps, std::vector<double>& azimuths, cv::Mat& fft, int range_bins = 3768);
 void PolarToCartesian(const std::vector<double>& azimuths, const cv::Mat fft, cv::Mat& cartesian);
+std::vector<std::string> getPathDir(const std::string& filepath);
+void getNRadar(const std::string& filepath, std::vector<RadarFFT>& radar_ffts, 
+              std::vector<RadarCartesian>& radar_carts, const int& N);
 
 #endif // RADAR_UTILS_HPP
